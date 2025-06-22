43ef49a2ef22a9d521c41a364beeccb87f4c885c
Ahmet Bekir Arslanalp <ahmetbekirarslanalp@gmail.com>
2025-06-22 20:59:58 +0300

    readme updated

---
+++ se3506/network.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to create an empty Mininet object
+(without a topology object) and add nodes to it manually.
+"""
+
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def emptyNet():
+
+    "Create an empty network and add nodes to it."
+
+    net = Mininet( controller=None, waitConnected=True )
+
+    info( '*** Adding controller\n' )
+    net.addController(name= 'c0', controller=RemoteController, ip="172.22.228.42",port=6653 )
+
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.0.1' )
+    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1' )
+
+    info( '*** Creating links\n' )
+    net.addLink( h1, s1 )
+    net.addLink( h2, s1 )
+
+    info( '*** Starting network\n')
+    net.start()
+
+    h2.cmd('xterm -e python3 -m http.server &')
+    input()
+    h1.cmd('xterm -e google-chrome --kiosk --no-sandbox --autoplay-policy=no-user-gesture-required --app=http://10.0.0.2:8000/index.html &')
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    emptyNet()
